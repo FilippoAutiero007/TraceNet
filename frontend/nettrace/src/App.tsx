197592ae0aa6c715b830f19b0212a6ed8b7b13cf
Filippo <filippoautiero07@ggmail.com>
2026-03-03 17:25:09 +0100

    Wrap app in HelmetProvider

--- frontend/nettrace/src/App.tsx
+++ frontend/nettrace/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { Navigation } from '@/components/Navigation';
 import { Landing } from '@/pages/Landing';
 import { Generator } from '@/pages/Generator';
@@ -8,15 +9,17 @@ import './App.css';
 function App() {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <div className="min-h-screen bg-slate-950">
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/generator" element={<Generator />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <HelmetProvider>
+        <BrowserRouter>
+          <div className="min-h-screen bg-slate-950">
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/generator" element={<Generator />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </HelmetProvider>
     </ErrorBoundary>
   );
 }
